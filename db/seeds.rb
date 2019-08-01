8e93ca1fe2ed6f9302f035e6bf7be750378a7d06
irinastephanie <stephanieiirn@gmail.com>
2019-08-02 00:50:07 +0200

    final version

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+Dog.destroy_all
+Dogsitter.destroy_all
+Stroll.destroy_all
+City.destroy_all
+
+10.times do
+  city = City.create!(city_name: Faker::Address.city)
+end
+
+
+20.times do 
+  dogsitter = Dogsitter.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, city: City.find(rand(City.first.id..City.last.id)))
+  dog = Dog.create!(name: Faker::Name.name, race: Faker::Creature::Dog.breed, city: City.find(rand(City.first.id..City.last.id)))
+end
+30.times do
+  stroll = Stroll.create!(date: Faker::Date.forward(days:28), dog: Dog.find(rand(Dog.first.id..Dog.last.id)), dogsitter: Dogsitter.find(rand(Dogsitter.first.id..Dogsitter.last.id)), city: City.find(rand(City.first.id..City.last.id)))
+end 
